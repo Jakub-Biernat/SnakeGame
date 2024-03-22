ea55a7a6607dab6d26ed5c45c2ffcfe87b48ca14
Jakub-Biernat <k.biernat@vp.pl>
2024-03-22 15:55:29 +0100

    Added Keypress to snake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from snake import Snake
 import time
+GAME_SPEED = 0.05
 
 my_screen = Screen()
 my_screen.setup(width=600, height=600)
@@ -10,9 +11,16 @@ my_screen.tracer(0)
 
 
 snake = Snake()
+my_screen.listen()
+my_screen.onkey(snake.up, "w")
+my_screen.onkey(snake.down, "s")
+my_screen.onkey(snake.left, "a")
+my_screen.onkey(snake.right, "d")
+
+
 while True:
     my_screen.update()
-    time.sleep(0.1)
+    time.sleep(GAME_SPEED)
     snake.move()
 
 
