cb6472670c56ca7bdaf251f551fd94455792c4cd
Jakub-Biernat <k.biernat@vp.pl>
2024-03-23 11:21:26 +0100

    Final touch

--- main.py
+++ main.py
@@ -37,10 +37,8 @@ while is_running:
         scoreboard.game_over()
         is_running = False
 
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             is_running = False
             scoreboard.game_over()
 
